e391dbac118e5dfd741eeb3ec018dfefbbdd77ed
Hannah Ward <Hannah.ward9001@gmail.com>
2016-05-13 20:54:50 +0100

    Authentication method implemented

---
+++ hftp-cli/hftpc.py
@@ -0,0 +1,115 @@
+import argparse
+import requests
+import rsa
+import sys
+import os
+import random
+import binascii
+
+from floatingutils.log import Log
+
+log = Log()
+
+
+parser = argparse.ArgumentParser(description='Connect to a HFTP server.')
+
+parser.add_argument('host', help='The address of the server')
+parser.add_argument('--port', default=9072,
+                    help='The port to access')
+parser.add_argument("--keydir", default=os.path.expanduser("~/.hftp"), 
+                    help="The directory containing your RSA keys")
+
+args = parser.parse_args()
+
+log.info("Using Server {}:{}".format(args.host, args.port))
+log.info("Using {}".format(args.keydir))
+
+
+try:
+  if not os.path.exists(args.keydir):
+    log.info("{} does not exist. Creating...".format(args.keydir))
+    os.mkdir(args.keydir)
+    raise FileNotFoundError
+  with open("{}/private.pem".format(args.keydir), "r") as f:
+    PRIVATE_KEY = rsa.PrivateKey.load_pkcs1(f.read())
+  with open("{}/public.pem".format(args.keydir), "r") as f:
+    PUBLIC_KEY = rsa.PublicKey.load_pkcs1(f.read())
+except FileNotFoundError as e:
+  log.info("Could not find one of your keys -- {}".format(e))
+  log.info("Generating keys...")
+  PUBLIC_KEY,PRIVATE_KEY = rsa.newkeys(512)
+  log.info("Saving keys to {}".format(args.keydir))
+
+  with open("{}/private.pem".format(args.keydir), "w") as f:
+    f.write(str(PRIVATE_KEY.save_pkcs1(), 'utf-8'))
+  with open("{}/public.pem".format(args.keydir), "w") as f:
+    f.write(str(PUBLIC_KEY.save_pkcs1(), 'utf-8'))
+
+log.info("Keys succesfully loaded.")
+
+sock = "http://{}:{}".format(args.host, args.port)
+log.info("Connecting to {}...".format(sock))
+
+r = requests.post("{}/auth".format(sock), data = {"request":"HELLO",
+                                        "pubkey":str(PUBLIC_KEY.save_pkcs1(), 'utf-8')})
+
+resp = r.text
+try:
+  auth_resp,srv_key,session_key = resp.split("\t\n")
+except ValueError:
+  log.error("Server response was invalid")
+  log.error("We got\n{}".format(resp))
+  sys.exit(1)
+
+if auth_resp != "HELLO_FRIEND":
+  log.error("The server responded with some weird auth string")
+  log.error("I wouldn't recommend connecting to it")
+  log.error(resp)
+  sys.exit(1)
+
+log.info("Recieved server key, checking it is who it says it is")
+
+SERV_KEY = rsa.PublicKey.load_pkcs1(srv_key)
+
+rand = random.Random()
+
+VERIFICATION_INTEGER = rand.randint(1, 1000000)
+enc = rsa.encrypt(bytes(str(VERIFICATION_INTEGER), 'utf-8'), SERV_KEY)
+enc = binascii.hexlify(enc)
+r = requests.post("{}/auth".format(sock), data = {"request":"LOOKIE", 
+                                                  "number":enc,
+                                                  "session":session_key}
+                  )
+resp = (r.text).split("\t\n")
+if resp[0] == "DECRYPTION FAILED - CHECK IT ALL PLS":
+  log.error("Server could not decrypt.")
+  log.error("It says we sent {}".format(bytes(resp[1], 'utf-8')))
+  sys.exit(1)
+else:
+  plusone = resp[1][2:-1]
+  number = int(rsa.decrypt(binascii.unhexlify(plusone), PRIVATE_KEY))
+  if number != VERIFICATION_INTEGER+1:
+    log.error("Server sent back the wrong number!")
+    sys.exit(1)
+  log.info("Server authenticated itself to us...")
+
+assert(resp[2] == "NOW_U")
+
+server_integer = binascii.unhexlify(resp[3][2:-1])
+server_integer = int(rsa.decrypt(server_integer, PRIVATE_KEY))
+
+addone = server_integer+1
+addone = binascii.hexlify(rsa.encrypt(bytes(str(addone),'utf-8'), SERV_KEY))
+
+r = requests.post("{}/auth".format(sock), data = {
+                                             "request":"I_LOOKED",
+                                             "session":session_key,
+                                             "number":addone}
+            )
+
+if r.text == "WE_COOL":
+  log.info("Server has accepted our credentials!")
+else:
+  log.error("The server rejected us :(")
+  sys.exit(1)
+
---
+++ hftp-srv/hftpd.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+import socketserver
+import rsa
+import os
+import cgi
+import random
+import string
+import binascii
+
+from floatingutils.conf import YamlConf
+from floatingutils.log  import Log
+
+log = Log()
+config = YamlConf("hftpd.conf")
+
+IP = config.getValue("server", "ip")
+PORT = config.getValue("server", "port")
+
+log.info("Loading RSA keys...")
+keydir = os.path.expanduser("~/.hftpd")
+
+rand = random.Random()
+
+try:
+  if not os.path.exists(keydir):
+    log.info("{} does not exist. Creating...".format(keydir))
+    os.mkdir(keydir)
+    raise FileNotFoundError
+  with open("{}/private.pem".format(keydir), "r") as f:
+    PRIVATE_KEY = rsa.PrivateKey.load_pkcs1(f.read())
+  with open("{}/public.pem".format(keydir), "r") as f:
+    PUBLIC_KEY = rsa.PublicKey.load_pkcs1(f.read())
+except FileNotFoundError as e:
+  log.info("Could not find one of your keys -- {}".format(e))
+  log.info("Generating keys...")
+  PUBLIC_KEY,PRIVATE_KEY = rsa.newkeys(512)
+  log.info("Saving keys to {}".format(keydir))
+                           
+  with open("{}/private.pem".format(keydir), "w") as f:
+    f.write(str(PRIVATE_KEY.save_pkcs1(), 'utf-8'))
+  with open("{}/public.pem".format(keydir), "w") as f:
+    f.write(str(PUBLIC_KEY.save_pkcs1(), 'utf-8'))
+                           
+log.info("Keys succesfully loaded.")
+
+class Session:
+  def __init__(self, ip, pubkey, sessionkey):
+    self.ip = ip 
+    self.pubkey = rsa.PublicKey.load_pkcs1(pubkey)
+    self.sessionkey = sessionkey
+
+  def encrypt(self, msg):
+    return str(rsa.encrypt(msg, self.pubkey), 'utf-8')
+
+  def getPub(self):
+    return self.pubkey
+
+  def setCheck(self, i):
+    self.verification = i
+
+  def getCheck(self):
+    return self.verification
+
+sessions = {}
+
+def genSessionKey():
+  key = ""
+  for i in range(20):
+    key += random.choice(string.ascii_letters)
+  return key
+
+class HFTPD(BaseHTTPRequestHandler):
+    
+  def do_GET(self):
+    log.info("Processing GET Req from {}".format(self.client_address))
+
+    log.info("Sending response...")
+   
+    log.info("Reqest asked for {}".format(self.path)) 
+  
+    resp = 404
+    msg = "WE ONLY DO POST SORRY FAM"    
+
+    self.send_response(resp)
+    
+    self.send_header("content-type", "text/plain")
+
+    self.end_headers()
+
+    self.wfile.write(self.fmt(msg))
+
+ 
+  def do_POST(self):
+    log.info("Processing POST Req from {}".format(self.client_address))
+
+    form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],
+                     })
+    post = {}
+    ##Convert to dict 
+    log.line("+")
+    log.info("Post info:")
+    for i in form.keys():
+      post[i] = form.getvalue(i)
+      log.info("  {} : '{}'".format(i, form.getvalue(i)))
+    log.line("+")
+    resp = 100
+    msg = "REQUEST NOT RECOGNISED"
+
+    if post["request"] == "HELLO":
+      resp,msg = self.do_AUTH(post["pubkey"])
+    else:
+      log.debug(sessions)
+      if not post["session"] in sessions:
+        #Deny access
+        self.send_response(100)
+        self.send_header("content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(self.fmt("ACCESS DENIED - AUTH FIRST"))
+        return
+      
+    if post["request"] == "LOOKIE":
+      resp,msg = self.do_HANDSHAKE(post["number"], post["session"])
+
+    if post["request"] == "I_LOOKED":
+      resp,msg = self.do_FINALIZE(post["number"], post["session"])
+
+    self.send_response(resp)
+
+    self.send_header("content-type", "text/plain")
+
+    self.end_headers()
+
+    self.wfile.write(self.fmt("{}".format(msg)))
+    log.line()
+    
+    return 
+
+  def do_AUTH(self, key=None):
+    if not key:
+      return 100, "NO FRIEND THAT IS NOT A KEY"
+    newkey = genSessionKey()
+    sessions[newkey] = Session(self.client_address[0], key, newkey)
+    return 200, "HELLO_FRIEND\t\n{}\t\n{}".format(str(PUBLIC_KEY.save_pkcs1(), 'utf-8'),
+                                              newkey)
+
+  def do_HANDSHAKE(self, number, sessionkey):
+    number = binascii.unhexlify(number)
+    try:
+      decr = int(rsa.decrypt(number, PRIVATE_KEY))
+    except rsa.pkcs1.DecryptionError:
+      return 200, "DECRYPTION FAILED - CHECK IT ALL PLS\t\n{}".format(number)
+    log.info(decr)
+    
+    addone = bytes(str(decr + 1), 'utf-8')
+    addone = binascii.hexlify(rsa.encrypt(addone, sessions[sessionkey].getPub()))
+    newint = rand.randint(1, 1000000)
+    sessions[sessionkey].setCheck(newint)
+    newint = bytes(str(newint), 'utf-8')
+    newint = binascii.hexlify(rsa.encrypt(newint, sessions[sessionkey].getPub()))
+    return 200, "I_LOOKED\t\n{}\t\nNOW_U\t\n{}".format(addone, newint)
+
+  def do_FINALIZE(self, number, session):
+    expecting = sessions[session].getCheck()+1
+    
+    got = int(rsa.decrypt(binascii.unhexlify(number), PRIVATE_KEY))
+    
+    if expecting == got:
+      return 200, "WE_COOL"
+    else:
+      return 200, "NO_FAM"
+
+  def get_file(self):
+    return 200, "GETTING"
+
+  def fmt(self, msg):
+    return bytes(msg, 'utf-8')
+
+
+try:
+  log.info("Starting server on {}:{}".format(IP, PORT))
+
+  server = HTTPServer((IP, PORT), HFTPD)
+
+  log.info("Server running.")
+
+  log.line()
+
+  server.serve_forever()
+
+except KeyboardInterrupt:
+  log.newline()
+  log.line()
+  
+  log.warning("Keyboard interrupt recieved. Shutting down...")
+  server.socket.close()
+  log.info("Socket closed, exiting.")
+
